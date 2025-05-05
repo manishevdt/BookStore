72b86eea84f5429431fbd1c2a50e7a69a805c92d
manishevdt <manishrathore.evdt@gmail.com>
2025-05-05 20:00:04 +0530

    [Adding ] content in about

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const About = () => {
   return (
+    <>
     <div
-    className='text-blue-700 mt-14 justify-center '>About</div>
+   className="max-w-screen-2xl text-2xl font-bold container mx-auto md:px-20 px-4 text-center text-cyan-950 dark:bg-slate-900 dark:text-white mt-14">About</div>
+    <div className=' p-8 items-center justify-center text-center'>
+    One web page for every book ever published. It's a lofty but achievable goal.<br/>To build Open Library, we need hundreds of millions of book records, a wiki interface, and lots of people who are willing to contribute their time and effort to building the site.
+</div>
+<div className=' p-8 items-center justify-center text-center'>
+To date, we have gathered over 20 million records from a variety of large catalogs as well as single contributions, with more on the way.
+Open Library is an open project: the software is open, the data are open, the documentation is open, and we welcome your contribution. Whether you fix a typo, add a book, or write a widget--it's all welcome. We have a small team of fantastic programmers who have accomplished a lot, but we can't do it alone!
+</div>
+<div className="max-w-screen-2xl text-2xl font-bold container mx-auto md:px-20 px-4 text-center text-cyan-950 dark:bg-slate-900 dark:text-white mt-14">
+Vision
+</div>
+<div className=' p-8 items-center justify-center text-center'>
+The ultimate goal of the Open Library is to make all the published works of humankind available to everyone in the world. While large in scope and ambition, this goal is within our grasp. Achieving it will require the participation of librarians, authors, government officials and technologists.
+</div>
+<div className=' p-8 items-center justify-center text-center'>
+Imagine what a comprehensive, open library could be! A talented math whiz who lives in a rural community can explore the works of high math. An elderly person can have a large print edition of any book ever published. An innovative young scholar can publish a book directly to this great library on subjects that might not otherwise make it through the long and difficult publication process.
+</div>
+
+    </>
   )
 }
 
